11d30d1534f31161e8d2f6d920abe30a523a39a7
Erik Griffin <griffin.erik@gmail.com>
2021-08-21 13:17:11 -0600

    Setup basic url redirection.
    Added redis-py dependency and an .editorconfig

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, abort, redirect
+import redis as RedisConn
 
 app = Flask(__name__)
+redis = RedisConn.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+redis.set("test", "https://unitedmasters.com/")
 
 @app.route("/")
 def index():
-    return "<h1>Hello, world.</h1>"
+    return "<h1>Tny.</h1><p>A URL shortener.</p>"
+
+@app.route("/<string:url_hash>")
+def resolve_url(url_hash):
+    url = redis.get(url_hash)
+    if url:
+        return redirect(url)
+    else:
+        abort(404)
